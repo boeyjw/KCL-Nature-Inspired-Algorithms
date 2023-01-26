289fffde028d2749b0f690fe187b319bce04fc75
Boey <23478480+boeyjw@users.noreply.github.com>
2023-01-26 11:39:44 +0000

    upd: show total weight when with_weight=true

--- src/generic.jl
+++ src/generic.jl
@@ -1,9 +1,14 @@
 function code_to_label_edges(g; with_weight=true)
+    total_weight = 0
     for e = edges(g)
         println(
             label_for(g, e.src), " => ", label_for(g, e.dst), " ",
             with_weight ? join(["(", g[label_for(g, e.src), label_for(g, e.dst)], ")"]) : "", 
             "\n"
         )
+        total_weight = total_weight + g[label_for(g, e.src), label_for(g, e.dst)];
+    end
+    if with_weight
+        println("Total Weight: ", total_weight)
     end
 end
